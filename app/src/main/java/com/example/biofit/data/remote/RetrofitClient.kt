f6bba0a2eb1790d55091a55adca3d3085779c1c8
Lala <nmdang6767@gmail.com>
2025-03-26 20:24:36 +0700

    change ip address in RetrofitClient

--- app/src/main/java/com/example/biofit/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/biofit/data/remote/RetrofitClient.kt
@@ -2,23 +2,23 @@ package com.example.biofit.data.remote
 
 import com.google.android.gms.cast.CastStatusCodes.TIMEOUT
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
+/*import okhttp3.logging.HttpLoggingInterceptor*/
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    const val BASE_URL = "http://172.16.5.172:8080/"
+    const val BASE_URL = "http://172.16.5.80:8080/"
 
     val instance: ApiService by lazy {
-        val okHttpClient = OkHttpClient.Builder()
+        /*val okHttpClient = OkHttpClient.Builder()
             .connectTimeout(TIMEOUT.toLong(), TimeUnit.SECONDS)
             .readTimeout(TIMEOUT.toLong(), TimeUnit.SECONDS)
             .writeTimeout(TIMEOUT.toLong(), TimeUnit.SECONDS)
             .addInterceptor(HttpLoggingInterceptor().apply {
                 level = HttpLoggingInterceptor.Level.BODY
             })
-            .build()
+            .build()*/
 
         Retrofit.Builder()
             .baseUrl(BASE_URL)
